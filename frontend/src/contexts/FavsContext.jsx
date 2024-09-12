928da06f41b37a2e9487e5d4c77073a7f414a771
Loreto956 <lore956@gmail.com>
2024-09-11 23:01:27 -0300

    Arreglo Favoritos

--- frontend/src/contexts/FavsContext.jsx
+++ frontend/src/contexts/FavsContext.jsx
@@ -15,6 +15,7 @@ const ShopProvider = ({ children }) => {
     const [likedProducts, setLikedProducts] = useState([]);
 
     useEffect(() => {
+      if (currentUser) {
         const fetchProductos = async () => {
           try {
             const token = Cookies.get('token');
@@ -29,21 +30,64 @@ const ShopProvider = ({ children }) => {
             setHasError(true);
             setTimeout(() => {
               setShowError(false);
-            }, 5000);
+            }, 3000);
           } finally {
             setLoading(false);
           }
         };
-        fetchProductos();
-      }, [setProducts]);
 
-    const handleLike = (productId) => {
-        setLikedProducts(prevLikedProducts =>
-          prevLikedProducts.includes(productId)
-            ? prevLikedProducts.filter(id => id !== productId)
-            : [...prevLikedProducts, productId]
-        );
-    };
+        const fetchFavoritos = async () => {
+          try {
+            const token = Cookies.get('token')
+            const response = await axios.get(ENDPOINT.favoritos, {
+              headers: {
+                Authorization: `Bearer ${token}`
+              }
+            })
+            setLikedProducts(response.data.map(fav => fav.producto_id))
+          } catch (error) {
+            console.error("Error al cargar favoritos", error)
+          }
+        }
+        fetchProductos();
+        fetchFavoritos();
+      }
+    }, [currentUser]);
+
+    // const handleLike = (productId) => {
+    //     setLikedProducts(prevLikedProducts =>
+    //       prevLikedProducts.includes(productId)
+    //         ? prevLikedProducts.filter(id => id !== productId)
+    //         : [...prevLikedProducts, productId]
+    //     );
+    // };
+
+    const handleLike = async (productId) => {
+      const isLiked = likedProducts.includes(productId);
+
+      try {
+          const token = Cookies.get('token');
+          if (isLiked) {
+             
+              await axios.delete(`${ENDPOINT.eliminarFavorito}/${productId}`, {
+                  headers: {
+                      Authorization: `Bearer ${token}`
+                  }
+              });
+              setLikedProducts(prevLikedProducts => prevLikedProducts.filter(id => id !== productId));
+          } else {
+              
+              await axios.post(ENDPOINT.registerFavorite, { producto_id: productId }, {
+                  headers: {
+                      Authorization: `Bearer ${token}`
+                  }
+              });
+              setLikedProducts(prevLikedProducts => [...prevLikedProducts, productId]);
+          }
+      } catch (error) {
+          console.error("Error al actualizar favoritos:", error);
+      }
+  };
 
 
     return (
